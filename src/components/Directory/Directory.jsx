a911062152d5a0ca097cfef1f3e5d340b603e8a8
Cam Ninh <camninh224@gmail.com>
2020-12-28 10:38:41 +0700

    search box

---
+++ src/components/Directory/Directory.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+
+const people = [
+    "Siri",
+    "Alexa",
+    "Google",
+    "Facebook",
+    "Twitter",
+    "Linkedin",
+    "Sinkedin"
+];
+
+function Directory() {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+    const handleChange = event => {
+        setSearchTerm(event.target.value);
+    };
+    useEffect(() => {
+        const results = people.filter(person =>
+            person.toLowerCase().includes(searchTerm)
+        );
+        setSearchResults(results);
+    }, [searchTerm]);
+
+    return (
+        <div className="App">
+            <input
+                type="text"
+                placeholder="Search"
+                value={searchTerm}
+                onChange={handleChange}
+            />
+            <ul>
+                {searchResults.map(item => (
+                    <li>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Directory
---
+++ src/pages/HomePage/homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Directory from '../../components/Directory/Directory';
+import './homepage.scss'
+
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            <h1 className="title">Homepage</h1>
+            <Directory />
+        </div>
+
+    )
+}
+
+export default Homepage
